ceda3f3aca08d543b4ce8518db8e899d6a9625d4
William Koppelberger <koppelbw@gmail.com>
2016-11-29 00:10:10 -0500

    connect angular and nodejs complete

--- 01 Seed/assets/app/messages/message.component.ts
+++ 01 Seed/assets/app/messages/message.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, Output, EventEmitter } from "@angular/core";
+import { Component, Input, Output } from "@angular/core";
 
 import { Message } from "./message.model";
 import { MessageService } from "./message.service";
@@ -24,16 +24,17 @@ import { MessageService } from "./message.service";
 })
 export class MessageComponent {
     @Input() message: Message;
-    @Output() editClicked = new EventEmitter<string>();
-    color = 'red';
 
     constructor(private messageService: MessageService) {}
 
     onEdit() {
-        this.editClicked.emit('A new value');
+        this.messageService.editMessage(this.message);
     }
 
     onDelete() {
-        this.messageService.deleteMessage(this.message);
+        this.messageService.deleteMessage(this.message)
+            .subscribe(
+                result => console.log(result)
+            );
     }
 }
--- 01 Seed/routes/messages.js
+++ 01 Seed/routes/messages.js
@@ -37,4 +37,63 @@ router.post('/', function (req, res, next) {
         })
 });
 
+router.patch('/:id', function (req, res, next) {
+    Message.findById(req.params.id, function(err, message) {
+        if(err) {
+            return res.status(500).json({
+                    title: 'An error occured!!',
+                    error: err
+                });
+        }
+        if(!message) {
+            return res.status(500).json({
+                    title: 'NO Message found!',
+                    error: {message: 'Message not found'}
+                });
+        }
+        message.content = req.body.content;
+        message.save(function(err, result) {
+            if(err) {
+                return res.status(500).json({
+                    title: 'An error occured!!',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Updated message',
+                obj: result
+            });
+        });
+    });
+});
+
+router.delete('/:id', function(req, res, next) {
+    Message.findById(req.params.id, function(err, message) {
+        if(err) {
+            return res.status(500).json({
+                    title: 'An error occured!!',
+                    error: err
+                });
+        }
+        if(!message) {
+            return res.status(500).json({
+                    title: 'NO Message found!',
+                    error: {message: 'Message not found'}
+                });
+        }
+        message.remove(function(err, result) {
+            if(err) {
+                return res.status(500).json({
+                    title: 'An error occured!!',
+                    error: err
+                });
+            }
+            res.status(200).json({
+                message: 'Deleted message',
+                obj: result
+            });
+        });
+    });
+});
+
 module.exports = router;
